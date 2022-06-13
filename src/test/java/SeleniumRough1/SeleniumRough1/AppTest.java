16511669bc8780cadf57062fb556b2a2248a0c0a
priyankaIBM <priyanka.joshi5@ibm.com>
2022-06-13 12:24:23 +0530

    initial commit officialBranch

---
+++ src/test/java/SeleniumRough1/SeleniumRough1/AppTest.java
@@ -0,0 +1,17 @@
+package SeleniumRough1.SeleniumRough1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/SeleniumRough1/SeleniumRough1/SeleniumTest1.java
@@ -0,0 +1,12 @@
+package SeleniumRough1.SeleniumRough1;
+
+public class SeleniumTest1 
+{
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		
+		
+	}
+	
+}
