43ee308138c81f0be390fdf9c84c0d005dd011c5
Your Name <you@example.com>
2024-03-28 11:20:39 +0530

    adding trial package

---
+++ Rough/src/Trial/Rough.java
@@ -0,0 +1,20 @@
+package Trial;
+
+import org.testng.annotations.Test;
+
+public class Rough {
+	
+	@Test
+	public void mtd() {
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
